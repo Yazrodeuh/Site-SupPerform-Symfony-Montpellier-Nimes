97c3d18848b4eca6d3feb70c12b269c6fa4beb32
maxime <maxou.pepette@gmail.com>
2021-10-06 20:21:00 +0200

    update

--- src/Data/ListeActus.php
+++ src/Data/ListeActus.php
@@ -9,27 +9,27 @@ class ListeActus
         [
             "titreActus" => "Inscriptions Terminale Santé",
             "contenuActus" => "Début des cours en elearning la semaine du 21 septembre. Stage de méthodologie la première semaine des vacances de la Toussaint. Pour tout renseignement : <a href=\"mailto:contact@sup-perform.fr\">contact@sup-perform.fr</a>",
-            "dateActus" => "2021-08-01",
+            "dateActus" => "2021-09-01", /* catégorie : terminale*/
         ],[
             "titreActus" => " Les résultats PASS 2020/2021",
             "contenuActus" => "Retrouvez les résultats sur <a href='/resultats'>la page consacrée</a>.",
-            "dateActus" => "2021-08-01",
+            "dateActus" => "2021-08-29", /*catégorie : pass */
         ],[
             "titreActus" => "Salon de l'étudiant",
             "contenuActus" => "Venez nous rencontrer au salon Santé, social, paramédical et sport au Corum le samedi 20 novembre. Plus d'infos sur la <a>page consacrée</a>.",
-            "dateActus" => "2021-08-01",
+            "dateActus" => "2021-10-01", /* catégorie : sup */
         ],[
             "titreActus" => "Inscription Première santé",
             "contenuActus" => "Stage spécifique méthodo les 25,26 et 27 octobre (1ere semaine des vacances de la Toussaint). Plus d'infos sur <a href='/formations/premiere-sante'>la première santé</a>",
-            "dateActus" => "2021-08-01",
+            "dateActus" => "2021-09-25", /* catégorie : première */
         ],[
             "titreActus" => "Information",
             "contenuActus" => "la réforme des études de santé. PASS/LAS, quelle filière choisir. Plus d'informations <a href='/reforme'>ici</a>.",
-            "dateActus" => "2021-08-01",
+            "dateActus" => "2021-10-05", /* catégorie : sup */
         ],[
             "titreActus" => "Inscriptions LAS",
             "contenuActus" => "Il est encore possible de s'inscrire en LAS. Pour cela : <a href=\"mailto:contact@sup-perform.fr\">contact@sup-perform.fr</a>. Plus d'infos sur les <a href='/formations/prepa-las'>programmes de préparation</a>.",
-            "dateActus" => "2021-08-01",
+            "dateActus" => "2021-09-15", /* catégorie : las */
         ],
 
     ];
